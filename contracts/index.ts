576bfd739b61f72ac0bd41a1e57d4c01d5b3e86f
tequ <git@tequ.dev>
2024-05-20 19:50:51 +0900

    esbuild

--- contracts/index.ts
+++ contracts/index.ts
@@ -1,11 +1,17 @@
 // @ts-nocheck
+import { ttINVOKE } from "./utils/tts"
+
+const transactiontype = () => otxn_type()
+
 const Hook = (arg) => {
   trace('HookOnTT.js: Called.', false)
   // _g(1, 1)
 
-  const tt = otxn_type()
-  if (tt !== 99) {
+  const tt = transactiontype()
+  if (tt !== ttINVOKE) {
     return rollback('hook_on_tt: HookOn field is incorrectly set.', -37)
   }
   return accept('HookOnTT.js: Finished.', 13)
 }
+
+export { Hook }
